f88069cae329e8504bc79dec2fa9b641adb1eac4
sbriggssjc <sbriggssjc@gmail.com>
2025-08-08 14:27:00 -0500

    Lower default stream resolution to 426x240

--- start_stream.sh
+++ start_stream.sh
@@ -53,10 +53,10 @@ mkdir -p "$LOG_DIR"
 LOG_FILE="$LOG_DIR/start_stream_$(date +%Y%m%d_%H%M%S).log"
 
 cmd=(ffmpeg -loglevel verbose \
-    -f v4l2 -framerate 30 -video_size 640x360 -i /dev/video0 \
+    -f v4l2 -framerate 30 -video_size 426x240 -i /dev/video0 \
     -f lavfi -i anullsrc=channel_layout=stereo:sample_rate=44100 \
     -c:v libx264 -preset veryfast -pix_fmt yuv420p \
-    -b:v 13500k -maxrate 13500k -bufsize 27000k -g 60 \
+    -b:v 2000k -maxrate 3000k -bufsize 4000k -g 60 \
     -c:a aac -b:a 128k \
     -f flv "$YOUTUBE_URL")
 
